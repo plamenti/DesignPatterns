3a9c123118f714327e82e20d54854bdd3e03b175
plamenti <plamenti_@abv.bg>
2018-07-04 09:05:18 +0300

    Add behaviors

---
+++ Strategy/src/com/plamenti/Interfaces/FlyBehavior.java
@@ -0,0 +1,5 @@
+package com.plamenti.Interfaces;
+
+public interface FlyBehavior{
+    void fly();
+}
---
+++ Strategy/src/com/plamenti/Interfaces/QuackBehavior.java
@@ -0,0 +1,5 @@
+package com.plamenti.Interfaces;
+
+public interface QuackBehavior{
+    void quack();
+}
