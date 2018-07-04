a0d86f9cdb47e09bc7a502b4e4f36928b90b18d5
plamenti <plamenti_@abv.bg>
2018-07-04 09:11:01 +0300

    Add parent abstract class with behaviors

---
+++ Strategy/src/com/plamenti/Duck.java
@@ -0,0 +1,33 @@
+package com.plamenti;
+
+import com.plamenti.Interfaces.FlyBehavior;
+import com.plamenti.Interfaces.QuackBehavior;
+
+public abstract class Duck{
+    private FlyBehavior flyBehavior;
+    private QuackBehavior quackBehavior;
+
+
+
+    public void setFlyBehavior(FlyBehavior flyBehavior){
+        this.flyBehavior = flyBehavior;
+    }
+
+    public void setQuackBehavior(QuackBehavior quackBehavior){
+        this.quackBehavior = quackBehavior;
+    }
+
+    public abstract void Display();
+
+    public void performFly(){
+        this.flyBehavior.fly();
+    }
+
+    public void performQuack(){
+        this.quackBehavior.quack();
+    }
+
+    public void swim(){
+        System.out.println("All ducks can swim!");
+    }
+}
