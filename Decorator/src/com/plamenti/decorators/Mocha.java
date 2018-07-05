5f693768c350f60e3b3b8e8b2d3cf1a9066d3826
plamenti <plamenti_@abv.bg>
2018-07-05 19:24:22 +0300

    Create Mocha class - decorator

---
+++ Decorator/src/com/plamenti/decorators/Mocha.java
@@ -0,0 +1,23 @@
+package com.plamenti.decorators;
+
+import com.plamenti.beveragesToBeDecorated.Beverage;
+
+public class Mocha extends CondimentDecorator{
+    public final double MOCHA_COST = 0.20;
+    public final String MOCHA_NAME = "Mocha";
+    Beverage beverage;
+
+    public Mocha(Beverage beverage){
+        this.beverage = beverage;
+    }
+
+    @Override
+    public String getDescription(){
+        return beverage.getDescription() + ", " + MOCHA_NAME;
+    }
+
+    @Override
+    public double cost(){
+        return beverage.cost() + MOCHA_COST;
+    }
+}
