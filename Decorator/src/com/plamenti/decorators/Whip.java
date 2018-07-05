67c9c20526f636c2062800e20c538d90692ba425
plamenti <plamenti_@abv.bg>
2018-07-05 19:29:33 +0300

    Create Whip class - decorator

---
+++ Decorator/src/com/plamenti/decorators/Whip.java
@@ -0,0 +1,23 @@
+package com.plamenti.decorators;
+
+import com.plamenti.beveragesToBeDecorated.Beverage;
+
+public class Whip extends CondimentDecorator{
+    public final double WHIP_COST = 0.10;
+    public final String WHIP_NAME = "Whip";
+    Beverage beverage;
+
+    public Whip(Beverage beverage){
+        this.beverage = beverage;
+    }
+
+    @Override
+    public String getDescription(){
+        return beverage.getDescription() + ", " + WHIP_NAME;
+    }
+
+    @Override
+    public double cost(){
+        return beverage.cost() + WHIP_COST;
+    }
+}
