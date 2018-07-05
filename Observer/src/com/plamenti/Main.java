92247805f707273e10f6da59a1ae0c2df98f256a
plamenti <plamenti_@abv.bg>
2018-07-05 10:39:48 +0300

    Create Observer and Observable implementations, test functionality

--- Observer/src/com/plamenti/Main.java
+++ Observer/src/com/plamenti/Main.java
@@ -1,8 +1,17 @@
 package com.plamenti;
 
+import com.plamenti.Observers.CurrentConditionsDisplay;
+import com.plamenti.Subject.WeatherData;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        WeatherData weatherData = new WeatherData();
+
+        CurrentConditionsDisplay currentConditionsDisplay = new CurrentConditionsDisplay(weatherData);
+
+        weatherData.setMeasurements(22, 75, 1024);
+        weatherData.setMeasurements(23, 65, 1000);
+        weatherData.setMeasurements(24, 70, 980);
     }
 }
