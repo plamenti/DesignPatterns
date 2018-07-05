719f47e4384adc5c64b421df06052efab0d3f511
plamenti <plamenti_@abv.bg>
2018-07-05 19:10:47 +0300

    Create HouseBlend class - class that can be decorated

---
+++ Decorator/src/com/plamenti/beveragesToBeDecorated/HouseBlend.java
@@ -0,0 +1,12 @@
+package com.plamenti.beveragesToBeDecorated;
+
+public class HouseBlend extends Beverage{
+    public HouseBlend(){
+        description = "House Blend coffee";
+    }
+
+    @Override
+    public double cost(){
+        return 0.89;
+    }
+}
