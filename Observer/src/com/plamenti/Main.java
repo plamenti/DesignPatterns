8ff17a8d993815033164adb67b4c4170b1899e7e
plamenti <plamenti_@abv.bg>
2018-07-05 11:08:25 +0300

    Add more Observer implementations, test functionality

--- Observer/src/com/plamenti/Main.java
+++ Observer/src/com/plamenti/Main.java
@@ -1,6 +1,7 @@
 package com.plamenti;
 
 import com.plamenti.Observers.CurrentConditionsDisplay;
+import com.plamenti.Observers.ForecastDisplay;
 import com.plamenti.Subject.WeatherData;
 
 public class Main {
@@ -9,9 +10,11 @@ public class Main {
         WeatherData weatherData = new WeatherData();
 
         CurrentConditionsDisplay currentConditionsDisplay = new CurrentConditionsDisplay(weatherData);
+        ForecastDisplay forecastDisplay = new ForecastDisplay(weatherData);
 
         weatherData.setMeasurements(22, 75, 1024);
         weatherData.setMeasurements(23, 65, 1000);
-        weatherData.setMeasurements(24, 70, 980);
+        weatherData.setMeasurements(24, 80, 980);
+        weatherData.setMeasurements(23, 65, 1000);
     }
 }
--- Observer/src/com/plamenti/Observers/ForecastDisplay.java
+++ Observer/src/com/plamenti/Observers/ForecastDisplay.java
@@ -6,6 +6,7 @@ import com.plamenti.Interfaces.IObserver;
 
 public class ForecastDisplay implements IObserver, IDisplay{
     private float temperature;
+    private float lastTemperature;
     private float humidity;
     private float lastHumidity;
     private float pressure;
@@ -15,6 +16,7 @@ public class ForecastDisplay implements IObserver, IDisplay{
     public ForecastDisplay(IObservable observableSubjectWeatherData){
         this.observableSubjectWeatherData = observableSubjectWeatherData;
         this.observableSubjectWeatherData.registerObserver(this);
+        this.temperature = 20;
         this.humidity = 75;
         this.pressure = 1000;
     }
@@ -24,21 +26,33 @@ public class ForecastDisplay implements IObserver, IDisplay{
         System.out.println( "### Weather forecast: ###");
         if(lastHumidity > humidity && lastPressure < pressure){
             System.out.println("The weather is going to be bad!");
+            System.out.println("Expected temperature: " + (this.temperature - 2));
+            updateMeasurements();
         } else if(lastHumidity == humidity && lastPressure == pressure){
             System.out.println("The weather is going to be the same!");
+            System.out.println("Expected temperature: " + this.temperature);
         } else if(lastHumidity < humidity && lastPressure > pressure){
             System.out.println("The weather is going to be better!");
+            System.out.println("Expected temperature: " + (this.temperature + 2));
+            updateMeasurements();
         } else{
             System.out.println("The weather is going to be the same but watch out for possible changes!");
             System.out.println("Just keep an eye on our forecast service!");
+            updateMeasurements();
         }
 
         System.out.println("#######");
     }
 
+    private void updateMeasurements(){
+        this.temperature = lastTemperature;
+        this.humidity = lastHumidity;
+        this.pressure = lastPressure;
+    }
+
     @Override
     public void update(float temperature, float humidity, float pressure){
-        this.temperature = temperature;
+        this.lastTemperature = temperature;
         this.lastHumidity = humidity;
         this.lastPressure = pressure;
         display();
