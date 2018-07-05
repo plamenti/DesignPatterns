e3a6c4e3eeed06dad207bcc57c0a93fad8eb8b8a
plamenti <plamenti_@abv.bg>
2018-07-05 19:51:16 +0300

    Create several orders of different beverages with condiments

--- Decorator/src/com/plamenti/Main.java
+++ Decorator/src/com/plamenti/Main.java
@@ -1,14 +1,39 @@
 package com.plamenti;
 
-import com.plamenti.beveragesToBeDecorated.Beverage;
-import com.plamenti.beveragesToBeDecorated.Espresso;
+import com.plamenti.beveragesToBeDecorated.*;
+import com.plamenti.decorators.Mocha;
+import com.plamenti.decorators.Soy;
+import com.plamenti.decorators.Whip;
 
 public class Main {
 
     public static void main(String[] args) {
         // Order some coffee without any condiment
-        Beverage beverage = new Espresso();
-        System.out.println(beverage.getDescription() + " $" + beverage.cost());
+        Beverage espresso = new Espresso();
+        printOrderDescription(espresso);
 
+        // Order Dark roasted coffee with Mocha and Whip condiments
+        Beverage darkRoastedCoffee = new DarkRoast();
+        darkRoastedCoffee = new Mocha(darkRoastedCoffee);
+        darkRoastedCoffee = new Whip(darkRoastedCoffee);
+        printOrderDescription(darkRoastedCoffee);
+
+        // Order House blend coffee with Mocha, Whip and Soy condiments
+        Beverage houseBlend = new HouseBlend();
+        houseBlend = new Mocha(houseBlend);
+        houseBlend = new Whip(houseBlend);
+        houseBlend = new Soy(houseBlend);
+        printOrderDescription(houseBlend);
+
+        // Order Decaffeinato coffee with Mocha condiment
+        Beverage decaffeinato = new Decaffeinato();
+        decaffeinato = new Mocha(decaffeinato);
+        printOrderDescription(decaffeinato);
+    }
+
+    private static void printOrderDescription(Beverage beverage){
+        System.out.println("###################");
+        System.out.println(beverage.getDescription() + " - $" + beverage.cost());
+        System.out.println("###################");
     }
 }
