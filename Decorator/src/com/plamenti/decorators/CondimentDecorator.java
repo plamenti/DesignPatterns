eee7f2e5a43e1f5c82c30b58bec5197792dcd865
plamenti <plamenti_@abv.bg>
2018-07-05 19:04:57 +0300

    Create abstract CondimentDecorator class - base class for all decorators

---
+++ Decorator/src/com/plamenti/decorators/CondimentDecorator.java
@@ -0,0 +1,8 @@
+package com.plamenti.decorators;
+
+import com.plamenti.beveragesToBeDecorated.Beverage;
+
+public abstract class CondimentDecorator extends Beverage{
+
+    public abstract String getDescription();
+}
