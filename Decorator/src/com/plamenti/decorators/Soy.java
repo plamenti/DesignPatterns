6344a284841bd4bb5ce14e0fd287837eb0fecbc6
plamenti <plamenti_@abv.bg>
2018-07-05 19:26:37 +0300

    Create Soy class - decorator

---
+++ Decorator/src/com/plamenti/decorators/Soy.java
@@ -0,0 +1,23 @@
+package com.plamenti.decorators;
+
+import com.plamenti.beveragesToBeDecorated.Beverage;
+
+public class Soy extends CondimentDecorator{
+    public final double SOY_COST = 0.15;
+    public final String SOY_NAME = "Soy";
+    Beverage beverage;
+
+    public Soy(Beverage beverage){
+        this.beverage = beverage;
+    }
+
+    @Override
+    public String getDescription(){
+        return beverage.getDescription() + ", " + SOY_NAME;
+    }
+
+    @Override
+    public double cost(){
+        return beverage.cost() + SOY_COST;
+    }
+}
