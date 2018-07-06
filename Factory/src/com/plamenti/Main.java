7b94bdc9c7db2dec407bfd10d6f2c709bce996ee
plamenti <plamenti_@abv.bg>
2018-07-06 09:32:10 +0300

    Test Factory Method

--- Factory/src/com/plamenti/Main.java
+++ Factory/src/com/plamenti/Main.java
@@ -1,5 +1,7 @@
 package com.plamenti;
 
+import com.plamenti.factoryMethod.ChicagoPizzaStore;
+import com.plamenti.factoryMethod.NYPizzaStore;
 import com.plamenti.simpleFactory.Pizza;
 import com.plamenti.simpleFactory.PizzaSimpleFactory;
 import com.plamenti.simpleFactory.PizzaStore;
@@ -8,15 +10,50 @@ public class Main {
 
     public static void main(String[] args) {
         // Test Simple Factory
-        PizzaSimpleFactory factory = new PizzaSimpleFactory();
-        PizzaStore store = new PizzaStore(factory);
+//        PizzaSimpleFactory factory = new PizzaSimpleFactory();
+//        PizzaStore store = new PizzaStore(factory);
+//
+//        Pizza pizza = store.orderPizza("cheese");
+//        System.out.println("Ordered pizza: " + pizza.getName());
+//        System.out.println(pizza);
+//
+//        pizza = store.orderPizza("clam");
+//        System.out.println("Ordered pizza: " + pizza.getName());
+//        System.out.println(pizza);
 
-        Pizza pizza = store.orderPizza("cheese");
-        System.out.println("Ordered pizza: " + pizza.getName());
-        System.out.println(pizza);
+        // Test Factory Method
+        com.plamenti.factoryMethod.PizzaStore chicagoStore = new ChicagoPizzaStore();
+        com.plamenti.factoryMethod.PizzaStore nyStore = new NYPizzaStore();
 
-        pizza = store.orderPizza("clam");
-        System.out.println("Ordered pizza: " + pizza.getName());
+        com.plamenti.factoryMethod.Pizza pizza = nyStore.orderPizza("cheese");
         System.out.println(pizza);
+        System.out.println("Ordered a " + pizza.getName() + "\n");
+
+        pizza = chicagoStore.orderPizza("cheese");
+        System.out.println(pizza);
+        System.out.println("Ordered a " + pizza.getName() + "\n");
+
+        pizza = nyStore.orderPizza("clam");
+        System.out.println(pizza);
+        System.out.println("Ordered a " + pizza.getName() + "\n");
+
+        pizza = chicagoStore.orderPizza("clam");
+        System.out.println("Ordered a " + pizza.getName() + "\n");
+
+        pizza = nyStore.orderPizza("pepperoni");
+        System.out.println(pizza);
+        System.out.println("Ordered a " + pizza.getName() + "\n");
+
+        pizza = chicagoStore.orderPizza("pepperoni");
+        System.out.println(pizza);
+        System.out.println("Ordered a " + pizza.getName() + "\n");
+
+        pizza = nyStore.orderPizza("veggie");
+        System.out.println(pizza);
+        System.out.println("Ordered a " + pizza.getName() + "\n");
+
+        pizza = chicagoStore.orderPizza("veggie");
+        System.out.println(pizza);
+        System.out.println("Ordered a " + pizza.getName() + "\n");
     }
 }
