78b78f5418f9afe620dd9cedbb4bc0347c81a2d8
plamenti <plamenti_@abv.bg>
2018-07-06 10:38:57 +0300

    Add Sauce ingredient implementations

---
+++ Factory/src/com/plamenti/abstractFactory/ingredients/MarinaraSauce.java
@@ -0,0 +1,7 @@
+package com.plamenti.abstractFactory.ingredients;
+
+public class MarinaraSauce implements Sauce{
+    public String toString() {
+        return "Marinara Sauce";
+    }
+}
---
+++ Factory/src/com/plamenti/abstractFactory/ingredients/PlumTomatoSauce.java
@@ -0,0 +1,7 @@
+package com.plamenti.abstractFactory.ingredients;
+
+public class PlumTomatoSauce implements Sauce{
+    public String toString() {
+        return "Tomato sauce with plum tomatoes";
+    }
+}
