3dcd88b7c0179c87e79f909d7d068d39977f88aa
plamenti <plamenti_@abv.bg>
2018-07-06 09:15:20 +0300

    Add NYStyleClamPizza class

---
+++ Factory/src/com/plamenti/factoryMethod/nyPizzas/NYStyleClamPizza.java
@@ -0,0 +1,14 @@
+package com.plamenti.factoryMethod.nyPizzas;
+
+import com.plamenti.factoryMethod.Pizza;
+
+public class NYStyleClamPizza extends Pizza{
+    public NYStyleClamPizza(){
+        name = "NY Style Clam Pizza";
+        dough = "Thin Crust Dough";
+        sauce = "Marinara Sauce";
+
+        toppings.add("Grated Reggiano Cheese");
+        toppings.add("Fresh Clams from Long Island Sound");
+    }
+}
