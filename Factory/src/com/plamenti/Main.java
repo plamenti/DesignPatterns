a23e7993c0d69dc0df1b772fee4b367eef084e6e
plamenti <plamenti_@abv.bg>
2018-07-06 08:38:39 +0300

    Test Simple factory

--- Factory/src/com/plamenti/Main.java
+++ Factory/src/com/plamenti/Main.java
@@ -1,8 +1,22 @@
 package com.plamenti;
 
+import com.plamenti.SimpleFactory.Pizza;
+import com.plamenti.SimpleFactory.PizzaSimpleFactory;
+import com.plamenti.SimpleFactory.PizzaStore;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // Test Simple Factory
+        PizzaSimpleFactory factory = new PizzaSimpleFactory();
+        PizzaStore store = new PizzaStore(factory);
+
+        Pizza pizza = store.orderPizza("cheese");
+        System.out.println("Ordered pizza: " + pizza.getName());
+        System.out.println(pizza);
+
+        pizza = store.orderPizza("clam");
+        System.out.println("Ordered pizza: " + pizza.getName());
+        System.out.println(pizza);
     }
 }
