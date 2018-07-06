53b24d1aa4c89539e2de9d22c1aea237d0ae2aa3
plamenti <plamenti_@abv.bg>
2018-07-06 08:20:05 +0300

    Add Pepperoni pizza class

---
+++ Factory/src/com/plamenti/SimpleFactory/PepperoniPizza.java
@@ -0,0 +1,12 @@
+package com.plamenti.SimpleFactory;
+
+public class PepperoniPizza extends Pizza{
+    public PepperoniPizza(){
+        name = "Pepperoni Piza";
+        dough = "Crust";
+        sauce = "Marinara sauce";
+        toppings.add("Sliced pepperoni");
+        toppings.add("Sliced onion");
+        toppings.add("Grated parmesan cheese");
+    }
+}
