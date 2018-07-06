9a5474d068f8c3767d6b3af3848077436a4bb2f5
plamenti <plamenti_@abv.bg>
2018-07-06 10:26:45 +0300

    Add Clams ingredient implementations

---
+++ Factory/src/com/plamenti/abstractFactory/ingredients/FrozenClams.java
@@ -0,0 +1,7 @@
+package com.plamenti.abstractFactory.ingredients;
+
+public class FrozenClams implements Clams{
+    public String toString() {
+        return "Frozen Clams from Chesapeake Bay";
+    }
+}
