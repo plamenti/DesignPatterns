3d24202d467e04809161b0c5596c9d4bf6fc9e8d
plamenti <plamenti_@abv.bg>
2018-07-07 08:23:27 +0300

    Create Cheese pizza

---
+++ Factory/src/com/plamenti/abstractFactory/pizzas/CheesePizza.java
@@ -0,0 +1,19 @@
+package com.plamenti.abstractFactory.pizzas;
+
+import com.plamenti.abstractFactory.ingredients.PizzaIngredientFactory;
+
+public class CheesePizza extends Pizza{
+    PizzaIngredientFactory pizzaIngredientFactory;
+
+    public CheesePizza(PizzaIngredientFactory pizzaIngredientFactory){
+        this.pizzaIngredientFactory = pizzaIngredientFactory;
+    }
+
+    @Override
+    public void prepare(){
+        System.out.println("Preparing " + name);
+        dough = pizzaIngredientFactory.createDough();
+        sauce = pizzaIngredientFactory.createSauce();
+        cheese = pizzaIngredientFactory.createCheese();
+    }
+}
