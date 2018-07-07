3a27d13c64fa169402181e9a45255f7b92a50779
plamenti <plamenti_@abv.bg>
2018-07-07 08:28:33 +0300

    Create Pepperoni pizza

---
+++ Factory/src/com/plamenti/abstractFactory/pizzas/PepperoniPizza.java
@@ -0,0 +1,21 @@
+package com.plamenti.abstractFactory.pizzas;
+
+import com.plamenti.abstractFactory.ingredients.PizzaIngredientFactory;
+
+public class PepperoniPizza extends Pizza{
+    PizzaIngredientFactory pizzaIngredientFactory;
+
+    public PepperoniPizza(PizzaIngredientFactory pizzaIngredientFactory){
+        this.pizzaIngredientFactory = pizzaIngredientFactory;
+    }
+
+    @Override
+    public void prepare(){
+        System.out.println("Preparing " + name);
+        dough = pizzaIngredientFactory.createDough();
+        sauce = pizzaIngredientFactory.createSauce();
+        cheese = pizzaIngredientFactory.createCheese();
+        veggies = pizzaIngredientFactory.createVeggies();
+        pepperoni = pizzaIngredientFactory.createPepperoni();
+    }
+}
