5e4f23ed0e94ee2ba200aade1bd5b595c35ffd1c
plamenti <plamenti_@abv.bg>
2018-07-07 08:39:53 +0300

    Create ChicagoPizzaStore

--- Factory/src/com/plamenti/abstractFactory/stores/ChicagoPizzaStore.java
+++ Factory/src/com/plamenti/abstractFactory/stores/ChicagoPizzaStore.java
@@ -1,27 +1,27 @@
 package com.plamenti.abstractFactory.stores;
 
-import com.plamenti.abstractFactory.ingredients.NYPizzaIngredientFactory;
+import com.plamenti.abstractFactory.ingredients.ChicagoPizzaIngredientFactory;
 import com.plamenti.abstractFactory.ingredients.PizzaIngredientFactory;
 import com.plamenti.abstractFactory.pizzas.*;
 
-public class NYPizzaStore extends PizzaStore{
+public class ChicagoPizzaStore extends PizzaStore{
     @Override
     protected Pizza createPizza(String type){
         Pizza pizza = null;
-        PizzaIngredientFactory pizzaIngredientFactory = new NYPizzaIngredientFactory();
+        PizzaIngredientFactory pizzaIngredientFactory = new ChicagoPizzaIngredientFactory();
 
         if(type.equals("cheese")){
             pizza = new CheesePizza(pizzaIngredientFactory);
-            pizza.setName("New York Style Cheese Pizza");
+            pizza.setName("Chicago Style Cheese Pizza");
         }else if(type.equals("veggie")){
             pizza = new VeggiePizza(pizzaIngredientFactory);
-            pizza.setName("New York Style Veggie Pizza");
+            pizza.setName("Chicago Style Veggie Pizza");
         }else if(type.equals("clam")){
             pizza = new ClamPizza(pizzaIngredientFactory);
-            pizza.setName("New York Style Clam Pizza");
+            pizza.setName("Chicago Style Clam Pizza");
         }else if(type.equals("pepperoni")){
             pizza = new PepperoniPizza(pizzaIngredientFactory);
-            pizza.setName("New York Style Pepperoni Pizza");
+            pizza.setName("Chicago Style Pepperoni Pizza");
         }
 
         return pizza;
